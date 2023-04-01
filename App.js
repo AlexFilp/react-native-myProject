155ad741c8ad001281a093b5343b91b4f79c3071
Oleksandr FIlippov <filya170895@gmail.com>
2023-04-01 18:49:59 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,59 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
+  Alert,
+  Button,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
-  console.log("HELLO");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [data, setData] = useState("");
+
+  const nameHandler = (name) => {
+    setUsername(name);
+  };
+
+  const passwordHandler = (pass) => {
+    setPassword(pass);
+  };
+
+  const onSubmit = () => {
+    Alert.alert("Credentials", `Username: ${username}; Password: ${password};`);
+  };
+
+  console.log(username);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <TextInput
+            style={styles.input}
+            value={username}
+            onChangeText={nameHandler}
+            placeholder="Username"
+          />
+          <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={passwordHandler}
+            placeholder="Password"
+          />
+          <Button title={"LOG IN"} style={styles.button} onPress={onSubmit} />
+        </KeyboardAvoidingView>
+
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -17,5 +63,31 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    ...Platform.select({
+      ios: {
+        backgroundColor: "lightgrey",
+      },
+      android: {
+        backgroundColor: "lightblue",
+      },
+    }),
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+    borderRadius: 100,
+  },
+  button: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+    borderRadius: 100,
   },
 });
