35853eea55c05dd664807052609dfae7eb6478d1
Oleksandr FIlippov <filya170895@gmail.com>
2023-04-02 18:28:38 +0200

    Update RegistrationScreen.jsx

--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -1,3 +1,16 @@
+import { StyleSheet, View, ImageBackground, Text } from "react-native";
+
 export const RegistrationScreen = () => {
-  return;
+  return (
+    <View style={styles.container}>
+      <ImageBackground></ImageBackground>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
