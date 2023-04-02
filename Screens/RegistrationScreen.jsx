32ec24bb3cdf411cdd7de52c0f148bf44f2f1a6d
Oleksandr FIlippov <filya170895@gmail.com>
2023-04-02 23:38:25 +0200

    Update RegistrationScreen.jsx

--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -11,11 +11,18 @@ import {
   useWindowDimensions,
   Dimensions,
 } from "react-native";
+import { useState } from "react";
 
 import { AntDesign } from "@expo/vector-icons";
 
 export const RegistrationScreen = () => {
-  const { width } = useWindowDimensions();
+  const [isPasswordHidden, setIsPasswordHidden] = useState(true);
+
+  // const { width } = useWindowDimensions();
+
+  const togglePassVisible = () => {
+    setIsPasswordHidden((prevState) => !prevState);
+  };
 
   return (
     <View style={styles.container}>
@@ -23,20 +30,66 @@ export const RegistrationScreen = () => {
         source={require("..//assets/images/mountainBg.jpg")}
         style={styles.bgImage}
       >
-        <View style={styles.form}>
+        <View style={styles.formContainer}>
           <View style={styles.avatarThumb}>
             <Image
               style={styles.avatar}
-              source={require("../assets/images/avatar.jpg")}
+              // source={require("../assets/images/avatar.jpg")}
             />
             <TouchableOpacity activeOpacity={0.8} style={styles.avatarBtn}>
               <AntDesign name="pluscircleo" size={25} color="#ff6c00" />
             </TouchableOpacity>
           </View>
-          <View style={{ width: width - 2 * 16 }}>
+          <View
+            style={{
+              marginHorizontal: 16,
+            }}
+            //  width: width - 16 * 2,
+          >
             <View style={styles.titleBox}>
               <Text style={styles.title}>Регистрация</Text>
             </View>
+            <View style={{ marginBottom: 16 }}>
+              <TextInput
+                placeholder="Логин"
+                placeholderTextColor={"#BDBDBD"}
+                style={{
+                  ...styles.input,
+                }}
+              />
+            </View>
+            <View style={{ marginBottom: 16 }}>
+              <TextInput
+                placeholder="Адрес электронной почты"
+                placeholderTextColor={"#BDBDBD"}
+                style={{
+                  ...styles.input,
+                }}
+              />
+            </View>
+            <View style={{ marginBottom: 43 }}>
+              <TextInput
+                placeholder="Пароль"
+                placeholderTextColor={"#BDBDBD"}
+                style={styles.input}
+                secureTextEntry={isPasswordHidden}
+              />
+              <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.passBtn}
+                onPress={togglePassVisible}
+              >
+                <Text style={styles.passText}>
+                  {isPasswordHidden ? "Показать" : "Скрыть"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.submitBtn}>
+              <Text style={styles.submitText}>Зарегистрироваться</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.link}>
+              <Text style={styles.linkText}>Уже есть аккаунт? Войти</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </ImageBackground>
@@ -52,13 +105,13 @@ const styles = StyleSheet.create({
   bgImage: {
     flex: 1,
     resizeMode: "cover",
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    // width: Dimensions.get("window").width,
+    // height: Dimensions.get("window").height,
     paddingTop: 263,
   },
-  form: {
+  formContainer: {
     flex: 1,
-    alignItems: "center",
+    // alignItems: "center",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
     backgroundColor: "#fff",
@@ -69,6 +122,7 @@ const styles = StyleSheet.create({
     height: 120,
     backgroundColor: "#F6F6F6",
     borderRadius: 16,
+    left: 128,
     top: -60,
     position: "absolute",
     zIndex: 1000,
@@ -85,7 +139,6 @@ const styles = StyleSheet.create({
   },
   titleBox: {
     alignItems: "center",
-    justifyContent: "center",
     marginBottom: 32,
   },
   title: {
@@ -94,5 +147,46 @@ const styles = StyleSheet.create({
     lineHeight: 35,
     letterSpacing: 0.01,
     color: "#212121",
+  },
+  input: {
+    backgroundColor: "#F6F6F6",
+    color: "#212121",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    fontSize: 16,
+    lineHeight: 19,
+    paddingHorizontal: 16,
+    height: 50,
+  },
+  passBtn: {
+    position: "absolute",
+    top: 16,
+    right: 16,
+  },
+  passText: {
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+  submitBtn: {
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    paddingVertical: 16,
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  submitText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  link: {
+    alignItems: "center",
+  },
+  linkText: {
+    color: "#1B4371",
+    fontSize: 16,
+    lineHeight: 19,
   },
 });
